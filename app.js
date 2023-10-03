c42f8f7e6f077cf68c4b698ecf212f1cab689c00
rawand-hawwari <rawandh3@gmail.com>
2023-10-03 16:10:27 +0300

    add the info to html file as p and ul

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 function order(){
-    // section 1
-    let name = String(prompt("Please enter your name"));
-
+    
     // section 2
     // let gender = String(prompt("Please enter your gender"));
     // let title;
@@ -34,10 +32,18 @@ function order(){
     //     title = "Ms";
     // }
 
+    
+    // section 1
+    let name = String(prompt("Please enter your name"));
+
     // ex 3 
     // instead of ex 2 for the previous section 
     // it take the value for gender and check it, but using a function that is called here
     let gender = String(prompt("Please enter your gender"));
+    let age = Number(prompt("Please enter your age"));
+    while(isNaN(age)){
+        age = Number(prompt("Something went wrong, please try again"));
+    }
     let title;
     gender = getGender(gender);
 
@@ -79,4 +85,29 @@ function order(){
     for(var i = 0;i<order.length;i++){
         console.log(order[i]);
     }
+
+    // DOM task
+    // create elements to add
+    let paragraph = document.createElement("p");
+    let list = document.createElement("ul");
+    let listItem1 = document.createElement("li");
+    let listItem2 = document.createElement("li");
+    var listItem3 = document.createElement("li");
+
+
+    // assign content for each element
+    paragraph.textContent = `${name}`;
+    listItem1.textContent = `Gender: ${gender}`;
+    listItem2.textContent = `Age: ${age}`;
+    listItem3.textContent = `Drink: ${drinkType} ${drinkName}`;
+
+    // add items to div to the last child
+    document.getElementById("dom").appendChild (paragraph);
+    document.getElementById("dom").appendChild (list);
+
+    // add id to the list then add list items in it
+    list.setAttribute('id','infoList');
+    document.getElementById("infoList").appendChild(listItem1);
+    document.getElementById("infoList").appendChild(listItem2);
+    document.getElementById("infoList").appendChild(listItem3);
 }
